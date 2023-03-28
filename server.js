d11cdd59415cb229401b367b0cb334fb85f8ca94
Isaiah Harris <imharris@unc.edu>
2023-03-28 16:02:06 -0400

    Add code to start server

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ var app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const args = minimist(process.argv.slice(2));
+
+var HTTP_PORT = 5000;
+
+if (args.port) {
+	HTTP_PORT = args.port;
+}
+
+// Start server
+const server = app.listen(HTTP_PORT, () => {
+	console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
+});
